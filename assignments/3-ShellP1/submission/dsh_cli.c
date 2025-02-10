bd620892cf2fb92c1b6914e3776f40d176e9fd6e
Loren <ll969@drexel.edu>
2025-02-10 11:30:46 -0500

    edits

--- assignments/3-ShellP1/submission/dsh_cli.c
+++ assignments/3-ShellP1/submission/dsh_cli.c
@@ -114,8 +114,11 @@ int main()
             // Print commands
             printf(CMD_OK_HEADER, clist.num);
             for (int i = 0; i < clist.num; i++) {
+
+                // If we have arguments
                 if (strlen(clist.commands[i].args) > 0) {
                     printf("<%d> %s [%s]\n", i + 1, clist.commands[i].exe, clist.commands[i].args);
+                    
                 } else {
                     printf("<%d> %s\n", i + 1, clist.commands[i].exe);
                 }
